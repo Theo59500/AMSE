6963a6d84cb4b3084156f0a82ccb3d1c784ab10a
Theo59500 <theoboussarie@gmail.com>
2022-03-03 19:49:11 +0100

    mise à jour de la page 'home'

--- tp2/lib/home.dart
+++ tp2/lib/home.dart
@@ -3,6 +3,11 @@ import 'exo1.dart';
 import 'exo2a.dart';
 import 'exo2b.dart';
 import 'exo4a.dart';
+import 'exo5a.dart';
+import 'exo5b.dart';
+import 'exo5c.dart';
+import 'exo6a.dart';
+import 'exo6b.dart';
 
 class Home extends StatelessWidget {
   @override
@@ -77,6 +82,96 @@ class Home extends StatelessWidget {
                   context,
                   MaterialPageRoute(
                       builder: (context) => Exo4a())
+              );
+            },
+          ),
+        ),
+        Card(
+          elevation: 8,
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10)
+          ),
+          child: ListTile(
+            title: Text("Exercice 5a"),
+            subtitle: Text("Grid of colored boxes"),
+            trailing: Icon(Icons.play_arrow),
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => Exo5a())
+              );
+            },
+          ),
+        ),
+        Card(
+          elevation: 8,
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10)
+          ),
+          child: ListTile(
+            title: Text("Exercice 5b"),
+            subtitle: Text("Fixed grid of cropped images"),
+            trailing: Icon(Icons.play_arrow),
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => Exo5b())
+              );
+            },
+          ),
+        ),
+        Card(
+          elevation: 8,
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10)
+          ),
+          child: ListTile(
+            title: Text("Exercice 5c"),
+            subtitle: Text("Configurable Taquin board"),
+            trailing: Icon(Icons.play_arrow),
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => Exo5c())
+              );
+            },
+          ),
+        ),
+        Card(
+          elevation: 8,
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10)
+          ),
+          child: ListTile(
+            title: Text("Exercice 6a"),
+            subtitle: Text("Moving tiles"),
+            trailing: Icon(Icons.play_arrow),
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => Exo6a())
+              );
+            },
+          ),
+        ),
+        Card(
+          elevation: 8,
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10)
+          ),
+          child: ListTile(
+            title: Text("Exercice 6b"),
+            subtitle: Text("Moving tiles in a board"),
+            trailing: Icon(Icons.play_arrow),
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => Exo6b())
               );
             },
           ),
